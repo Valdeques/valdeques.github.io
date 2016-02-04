2358e4e04367f67a7885baa02bae40133033d8c1
Valdeques Junior <valdeques@outlook.com>
2016-02-03 21:33:25 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 (function() {
-        var target_date = new Date("feb 6, 2016").getTime();
+        var target_date = new Date("feb 7, 2016").getTime();
          
         var days, hours, minutes, seconds, countdown = document.getElementById("countdown");
          
